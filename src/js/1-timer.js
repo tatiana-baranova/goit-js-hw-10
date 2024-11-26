715694dec42afb443b417bf1c1f188e895637e16
Tetiana Baranova <syniovych.tania@gmail.com>
2024-11-26 22:12:49 +0200

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,83 @@
-console.log("timer");
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+
+const btnStart = document.querySelector("[data-start]");
+let userSelectedDate = null;
+let timerId;
+
+const options = {
+enableTime: true,
+time_24hr: true,
+defaultDate: new Date(),
+minuteIncrement: 1,
+onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate <= new Date()) {
+    iziToast.warning({
+        title: 'Warning!',
+        message: 'Please choose a date in the future',
+    });
+    btnStart.disabled = true;
+    } else {
+    userSelectedDate = selectedDate;
+    btnStart.disabled = false;
+    }
+},
+};
+
+flatpickr("#datetime-picker", options);
+
+btnStart.addEventListener("click", function () {
+if (userSelectedDate) {
+    btnStart.disabled = true;
+
+    timerId = setInterval(() => {
+    const deltaTime = userSelectedDate - new Date();
+
+    
+    if (deltaTime <= 0) {
+        clearInterval(timerId);
+        updateTimer(0, 0, 0, 0);
+        iziToast.success({
+        title: "Success!",
+        message: "Timer has finished!",
+        });
+        return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    updateTimer(days, hours, minutes, seconds);
+    }, 1000);
+}
+});
+
+function convertMs(ms) {
+const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+const days = Math.floor(ms / day);
+const hours = Math.floor((ms % day) / hour);
+const minutes = Math.floor(((ms % day) % hour) / minute);
+const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+return { days, hours, minutes, seconds };
+}
+
+function updateTimer(days, hours, minutes, seconds) {
+document.querySelector("[data-days]").textContent = addLeadingZero(days);
+document.querySelector("[data-hours]").textContent = addLeadingZero(hours);
+document.querySelector("[data-minutes]").textContent = addLeadingZero(minutes);
+document.querySelector("[data-seconds]").textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+return String(value).padStart(2, "0");
+}
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,4 @@
-console.log("snackbar");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+console.log(iziToast);
+
