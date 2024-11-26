a7ec00c5223ee47cf787d857fd655a9600f168a3
tatiana-baranova <syniovych.tanya@gmail.com>
2024-11-26 20:13:24 +0000

    Deploying to gh-pages from @ tatiana-baranova/goit-js-hw-10@715694dec42afb443b417bf1c1f188e895637e16 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("snackbar");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as o}from"./assets/vendor-BbbuE1sJ.js";console.log(o);
 //# sourceMappingURL=2-snackbar.js.map
